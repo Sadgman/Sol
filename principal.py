2ac163707232c30a6f4bc9158db531ff40f7db22
angelluis24 <70178924+angelluis24@users.noreply.github.com>
2021-05-18 21:38:37 -0400

    Update principal.py

--- principal.py
+++ principal.py
@@ -6,42 +6,54 @@ class principal:
 
     def __init__(self, root):
 
-        self.frame = Frame(root, width=10080, height=1080)
-        self.frame.config(bg="#03A9F4")
+        root.geometry("1080x620")
+        root.minsize(width=200, height=256)
+
+        self.frame = Frame(root,bg="#03A9F4",width=10080, height=1080)
         self.frame.place(x=0, y=0)
-
-        self.menu = Menu(root)
-        root.config(menu=self.menu)
-
-        self.sub_menu = Menu(self.menu, tearoff=0)
-        self.sub_menu.add_separator()
-        self.sub_menu.add_command(label="salir", command=root.quit)
 
         self.imagen = PhotoImage(file="iconos e imagenes\hacia atras.png")
         self.imagen2 = PhotoImage(file="iconos e imagenes\hacia adelante.png")
+        self.imagen3 = PhotoImage(file="iconos e imagenes\salir.png")
+        self.imagen4 = PhotoImage(file="iconos e imagenes\Facturacion.png")
 
         self.boton_adelante = Button(root, image=self.imagen2, command=self.cambiar)
-        self.boton_adelante.place(x=900, y=300)
+        self.boton_adelante.place(rely=0.5,relx=0.9, x=16)
+        self.boton_adelante.place_configure(bordermode=OUTSIDE, anchor=SE)
 
-        self.boton_atras = Button(root, image=self.imagen)
-        self.boton_atras.place(x=900, y=500)
 
-        self.second_menu = Menu(self.menu)
-        self.menu.add_cascade(label="opciones", menu=self.sub_menu)
+        self.boton_atras = Button(root, image=self.imagen, command=root.destroy)
+        self.boton_atras.place(rely=0.8,relx=0.9, x=16)
+        self.boton_atras.place_configure(bordermode=OUTSIDE, anchor=SE)
 
-        self.imagen3 = PhotoImage(file="iconos e imagenes\salir.png")
+        self.boton_opcion_salir = Button(root, image=self.imagen3, bg="#0040FF", activebackground="#0040FF", command=root.destroy)
+        self.boton_opcion_facturacion = Button(root, image=self.imagen4, bg="#0040FF", activebackground="#0040FF")
 
-        self.boton_opcion = Button(root, image=self.imagen3, command=root.destroy)
+        menu = Menu(root)
+        root.config(menu=menu)
+
+        sub_menu = Menu(menu, tearoff=0)
+        sub_menu.add_separator()
+        sub_menu.add_command(label="salir", command=root.quit)
+
+        Menu(menu)
+        menu.add_cascade(label="opciones", menu=sub_menu)
 
     def cambiar(self):
 
         self.boton_atras.place_forget()
         self.boton_adelante.place_forget()
-        self.frame.place_forget()
-        self.boton_opcion.place(x=900, y=600)
+        self.boton_opcion_salir.place(rely=0.8,relx=0.9, x=16)
+        self.boton_opcion_salir.place_configure(bordermode=OUTSIDE, anchor=SE)
 
+        try:
 
-roto1 = Tk()
-roto1.geometry("1080x620")
-principal(roto1)
-roto1.mainloop()
+            self.boton_opcion_facturacion.place(rely=0.2,relx=0.001, x=16)
+            self.boton_opcion_facturacion.place_configure(bordermode=OUTSIDE, anchor=NO)
+
+        except tkinter.TclError:
+            pass
+
+root0 = Tk()
+principal(root0)
+root0.mainloop()
